ddc62cfa9c66074d75c541432462c9b30256cf28
laludash <laludash6@gmail.com>
2023-06-28 17:37:39 +0530

    UpdateCodeOne

---
+++ studentrestapi/src/main/java/studentrestapi/studentrepo/StudentRepo.java
@@ -0,0 +1,10 @@
+package studentrestapi.studentrepo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import studentrestapi.stduententity.Student;
+
+public interface StudentRepo extends CrudRepository<Student, Integer> {
+
+	
+}
